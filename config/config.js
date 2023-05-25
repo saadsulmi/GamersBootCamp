f50898fd8edb2c0bdbf85186fcfb86dc2d07bbad
saadsulmi <saadsulmi@gmail.com>
2023-05-25 23:06:48 +0530

    debug 3

--- config/config.js
+++ config/config.js
@@ -1,12 +1,6 @@
 
 const mongooseUp=()=>{
-    const mongoose=require('mongoose');
-    mongoose.set('strictQuery',false);
-    require('dotenv').config();
-    mongoose.connect(process.env.DATABASE);
-    mongoose.connection.once('open',()=>{
-    console.log('connection is successfully established');
-});
+  
 }
 
 module.exports={
