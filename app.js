96465e7b8807c9c26a5bc87e8c48beb4f303925f
saadsulmi <saadsulmi@gmail.com>
2023-05-27 12:04:10 +0530

    last update

--- app.js
+++ app.js
@@ -3,16 +3,11 @@ const nocache=require("nocache");
 const adminRoute=require('./routes/adminRoute');
 const userRoute=require('./routes/userRoute');
 const userForgetPass=require('./routes/forgotPassword');
+const mongo=require('./config/config');
 const app=express();
 const PORT=5000;
 //export mongoose it as function
-const mongoose=require('mongoose');
-mongoose.set('strictQuery',false);
-require('dotenv').config();
-mongoose.connect(process.env.DATABASE);
-mongoose.connection.once('open',()=>{
-console.log('connection is successfully established');
-});
+app.set(mongo.mongooseUp())
 
 app.set('view engine','ejs');
 
