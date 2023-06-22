e70fcb1047ecab8f9efafd28f4b0b3a0a44d20f6
Saad Sulmi M <62940044+saadsulmi@users.noreply.github.com>
2023-06-22 09:53:16 +0530

    Update twilio.js

--- middleware/twilio.js
+++ middleware/twilio.js
@@ -6,7 +6,7 @@ const AUTHTOKEN = '5308470d875e7e1d088cf678d0fb020c';
 const sendMessage=function (mobile,res,next){
     let randomOTP = Math.floor(Math.random() * 9000)+1000;
         twilio(ACCOUNTSID, AUTHTOKEN).messages.create({
-            body: 'Hello myre your otp is '+randomOTP ,
+            body: 'Hello your otp is '+randomOTP ,
             from: "Gamers Bootcamp", // Replace with your Twilio phone number
             to: mobile // Replace with the recipient's phone number
 })
